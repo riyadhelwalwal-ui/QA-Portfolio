e37e6325e9c56e4cb70fd3a109d91808ad2d2584
Riyadh <Riyadhelwalwal@gmail.com>
2026-08-31 21:29:56 +0200

    Add invalid login test cases

---
+++ tests/test_invalid_login.py
@@ -0,0 +1,24 @@
+import time
+from pages.login_page import LoginPage
+from utils import constants
+
+
+def test_login_with_invalid_credentials(browser):
+    # Schritt 1: Cookies löschen und Homepage öffnen
+    browser.delete_all_cookies()
+    browser.get(constants.BASE_URL)
+    time.sleep(2)
+
+    # Schritt 2: Login-Seite öffnen
+    login_page = LoginPage(browser)
+    login_page.click_profile()
+    time.sleep(2)
+
+    # Schritt 3: Ungültige Testdaten eingeben und absenden
+    login_page.enter_username(constants.INVALID_USER)
+    login_page.enter_password(constants.INVALID_PASSWORD)
+    login_page.login_buton()
+    time.sleep(3)
+
+    # Schritt 4: Verifikation - Sicherstellen, dass die Fehlermeldung erscheint
+    assert "Invalid" in browser.page_source or "error" in browser.page_source.lower()
